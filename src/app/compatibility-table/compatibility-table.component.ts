25cad7cb2e321b31789111d0964d0048e11fc27a
brandonha <brandonha@magenic.com>
2021-11-01 15:21:49 -0500

    Cumulative Compatibility introduced

--- src/app/compatibility-table/compatibility-table.component.ts
+++ src/app/compatibility-table/compatibility-table.component.ts
@@ -23,4 +23,17 @@ export class CompatibilityTableComponent implements OnInit {
     const choiceB = memberB.getChoiceGroup(this.preference.title);
     return choiceA.calculateCompatibility(choiceB);
   }
+
+  getCumulativeCompatibility(memberA: Member, memberB: Member): number {
+    const prefs: Preference[] = memberA.myChoices.map((x) => x.preference);
+    let compatibility = 0;
+    prefs.forEach((pref) => {
+      const choiceA = memberA.getChoiceGroup(pref.title);
+      const choiceB = memberB.getChoiceGroup(pref.title);
+      compatibility += choiceA.calculateCompatibility(choiceB);
+    });
+    compatibility /= prefs.length;
+
+    return compatibility;
+  }
 }
